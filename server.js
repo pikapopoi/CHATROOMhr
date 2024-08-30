b81fcae3cecb3c4f1e96b659e5fa45825f2d113b
harsh rathi <harsh1rathi1@gmail.com>
2024-08-31 01:00:47 +0530

    Updated app.js and styles.css to improve responsiveness and fix chat functionality

--- server.js
+++ server.js
@@ -1,24 +1,3 @@
-const express = require('express');
-const http = require('http');
-const socketIo = require('socket.io');
+const cors = require('cors');
+app.use(cors());
 
-const app = express();
-const server = http.createServer(app);
-const io = socketIo(server);
-
-app.use(express.static(__dirname));
-
-io.on('connection', (socket) => {
-    console.log('New user connected');
-
-    socket.on('chatMessage', (data) => {
-        io.emit('chatMessage', data);
-    });
-
-    socket.on('disconnect', () => {
-        console.log('User disconnected');
-    });
-});
-
-const PORT = process.env.PORT || 3000;
-server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
